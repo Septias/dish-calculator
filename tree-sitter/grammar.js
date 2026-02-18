de80764b77b0c23eb9c24e052410ce994dcd75c5
Sebastian Klähn <info@sebastian-klaehn.de>
2026-02-18 20:28:27 +0900

    paste in vibed grammar

--- tree-sitter/grammar.js
+++ tree-sitter/grammar.js
@@ -10,8 +10,98 @@
 module.exports = grammar({
   name: "menu",
 
+  extras: $ => [
+    /[ \t]/, // ignore spaces but NOT newlines
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ =>
+      seq(
+        $.persons_line,
+        "\n",
+        $.starttag_line,
+        repeat(seq("\n", $.day_line)),
+        optional("\n")
+      ),
+
+    // --------------------
+    // Header
+    // --------------------
+
+    persons_line: $ =>
+      seq("Personen:", field("count", $.integer)),
+
+    starttag_line: $ =>
+      seq("Starttag:", field("date", $.date)),
+
+    integer: _ => /\d+/,
+
+    date: _ => /\d{4}-\d{2}-\d{2}/,
+
+    // --------------------
+    // Day lines
+    // --------------------
+
+    day_line: $ =>
+      seq(
+        field("day", $.day_with_count),
+        ":",
+        optional($.menu)
+      ),
+
+    day_with_count: $ =>
+      seq(
+        field("name", $.day_name),
+        optional(field("count", $.count))
+      ),
+
+    // Accept anything until colon or newline
+    day_name: _ => /[^:\n(]+/,
+
+    count: _ => /\(\d+\)/,
+
+    // --------------------
+    // Menu
+    // --------------------
+
+    menu: $ =>
+      choice(
+        $.rest_day,
+        $.menu_items
+      ),
+
+    rest_day: _ => "Reste",
+
+    menu_items: $ =>
+      seq(
+        $.menu_item,
+        repeat(seq(",", $.menu_item))
+      ),
+
+    menu_item: $ =>
+      choice(
+        $.dish_with_count,
+        $.shopping_marker
+      ),
+
+    shopping_marker: _ =>
+      seq("⟨", /[^⟩]+/, "⟩"),
+
+    // --------------------
+    // Dishes
+    // --------------------
+
+    dish_with_count: $ =>
+      seq(
+        field("dish", $.dish),
+        optional(field("count", $.count))
+      ),
+
+    dish: $ =>
+      seq(
+        "[[",
+        field("name", /[^\]]+/),
+        "]]"
+      ),
   }
 });
---
+++ tree-sitter/queries/highlights.scm
@@ -0,0 +1,9 @@
+
+;; Personen count
+(integer) @number
+
+;; Counts like (3) or (12) after day or dish
+(count) @number
+
+;; Date numbers (whole date highlighted as number)
+(date) @number
