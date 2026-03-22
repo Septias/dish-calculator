0c8d8c21ba6dea29b1807e4c7c2ac855466a1692
Sebastian Klähn <info@sebastian-klaehn.de>
2026-03-22 15:10:31 +0100

    fix a bit in grammar

--- tree-sitter/dish/grammar.js
+++ tree-sitter/dish/grammar.js
@@ -2,59 +2,68 @@ module.exports = grammar({
   name: "dish",
 
   // Keep newlines significant; ignore only spaces and tabs
-  extras: $ => [/[\t ]+/],
+  extras: _ => [/[\t ]+/],
 
   rules: {
-
     source_file: $ =>
       seq(
         repeat("\n"),
         $.persons_line,
-        "\n",
-        repeat("\n"),
-        $.ingredients_section,
         repeat1("\n"),
-        $.preparation_section,
-        repeat("\n")
+        repeat(seq($.preamble_line, "\n")),
+        $.ingredients_section,
+        repeat("\n"),
+        optional($.preparation_section),
       ),
 
     persons_line: $ =>
       seq(
         field("count", $.integer),
         /[\t ]+/,
-        choice("Personen", "Portionen", "Portion")
+        choice("Personen", "Portionen")
       ),
 
     ingredients_section: $ =>
-      seq($.ingredients_header, "\n", repeat(seq($.ingredient_line, "\n"))),
+      prec.right(1, seq("## Zutaten", repeat1("\n"), repeat1(seq($.ingredient_line, "\n")))),
 
-    ingredients_header: _ => "## Zutaten",
 
     ingredient_line: $ =>
       seq(
         "-",
         /[\t ]+/,
-        field("quantity", $.quantity),
-        /[\t ]+/,
-        field("unit", $.unit),
-        /[\t ]+/,
-        field("name", $.ingredient_name)
+        optional(
+          seq(
+            field("quantity", $.quantity),
+            /[\t ]+/
+          )
+        ),
+        optional(
+          seq(
+            field("unit", $.unit),
+            /[\t ]+/
+          )
+        ),
+        field("name", $.ingredient_name),
+        optional(/[\t ]+/)  // Allow trailing spaces
       ),
 
     preparation_section: $ =>
-      seq($.preparation_header, "\n", repeat(seq($.step_line, "\n"))),
+      prec.right(1, seq("## Zubereitung", repeat1("\n"), repeat(seq($.step_line, "\n")), repeat("\n"))),
 
-    preparation_header: _ => "## Zubereitung",
 
     step_line: $ =>
-      seq(field("number", $.integer), ". ", field("text", $.text)),
+        seq(field("text", $.text))
+      ,
+
+    // Lines of free text that must not start with a section header (## ...)
+    preamble_line: _ => token(prec(1, /([^#][^\n\r]*|#[^#][^\n\r]*)/)),
 
     // Tokens
     quantity: $ => choice($.float, $.integer),
     integer: _ => /\d+/,
     float: _ => /\d+\.\d+/,
     unit: _ => /[^\s]+/,
-    ingredient_name: _ => /[^\n]*/,
-    text: _ => /[^\n]*/
+    ingredient_name: _ => /[^\n\r]*/,
+    text: _ => /[^\n\r]*/
   }
 });
